9e3feccf4b0d21f32bd9a16f3e127bc52ffc8eb0
May Kosaka <may.kosaka@gmail.com>
2016-06-10 16:44:11 -0700

    corrected AJAX movie project

--- ajax_exercise/movieMain.js
+++ ajax_exercise/movieMain.js
@@ -1,4 +1,4 @@
-/*
+/* OMDB project
 
 - Refactor the codealong to work with user interaction. In the index.html file
 there is a "Get Consumer Finance Data" button. When the user clicks the button,
@@ -9,6 +9,8 @@ console.log() if successful.
 - Separate your logic so that you can use your functions for another user button
 click of "Get Custom Data". Interact with an API of your choice and handle both
 success and error scenarios.
+- implement templating via handlebars to dynamically have your response object 
+populate your DOM.
 */
 
 /*'use strict';
@@ -17,46 +19,32 @@ success and error scenarios.
 })();
 */
 
-var source = $('#entryTemplate').html();
-var template = Handlebars.compile(source);
+var source = $('#entryTemplate').html(); // reference handlebars template in html file
+var template = Handlebars.compile(source); //reference the code/template(Handlebars), pass the object to it, compile the source markup
 
-// function for button to pass this url to the XMLHttpRequest
+// button event handler will pass this url to the XMLHttpRequest
 document.getElementById('movieBtn').onclick = function() {
-  makeRequest('http://www.omdbapi.com/?s=inception');
-  makeRequest('http://www.omdbapi.com/?s=clockwork-orange');
+  makeRequest('http://www.omdbapi.com/?s=clockwork-orange'); 
 }
-
-
 
 function makeRequest(url) {
   var httpRequest = new XMLHttpRequest()
 
   httpRequest.onreadystatechange = function(){ // state of API call, an EventHandler
     if(httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) { // if api call OK & server OK
-      var movieArray = JSON.parse(httpRequest.responseText)
-      
+      var movieArray = JSON.parse(httpRequest.responseText) //parse the data so it can be used
+
+      // loop through the data which is an array of movies
       for(var i = 0; i < movieArray.Search.length; i++){
         // define data object
         var context = {
-            //image: 'http://img.omdbapi.com/?i=' + <e46b1fbe>
             title: movieArray.Search[i].Title,
-            // voice 14. if issues can't tell where errors are, go into dev tools & check status
             year: movieArray.Search[i].Year,
-            imdb: movieArray.Search[i].imbdID,
             type: movieArray.Search[i].Type
         }
 
-/*OMBD movie poster
-API Key: e46b1fbe 
-
-Example: http://img.omdbapi.com/?i=tt2294629&apikey=e46b1fbe 
-
-Parameters:
-i = IMDb ID
-h (optional) = Desired height of image.*/
-
-        var html = template(context) // 
-        $('body').append(html) // 
+        var html = template(context) //pass data object to template
+        $('body').append(html) //attach template to DOM
       }
     }
   }
