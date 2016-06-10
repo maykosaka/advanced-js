b7fa1c59a6ff7de3e2bb44278ccd8e5732a89c99
May Kosaka <may.kosaka@gmail.com>
2016-06-10 16:46:30 -0700

    removed an irrelevant file

--- ajax_exercise/movieNotes.js
+++
@@ -1,8 +0,0 @@
-/*OMBD movie poster
-API Key: e46b1fbe
-poster api request: http://img.omdbapi.com/?apikey=[yourkey]&
-Example: http://img.omdbapi.com/?i=tt2294629&apikey=e46b1fbe
-
-Parameters:
-i = IMDb ID
-h (optional) = Desired height of image.*/
